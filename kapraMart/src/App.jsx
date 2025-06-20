0d8a709b11f410d9f42b60a9237f7a77b9de077c
Amit Kumar <kumaramit1996.1@gmail.com>
2025-06-20 23:45:52 +0530

    updated code

--- kapraMart/src/App.jsx
+++ kapraMart/src/App.jsx
@@ -2,15 +2,22 @@ import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
+import Navigation from "./customer/Navigation.jsx";
 
 function App()
 {
-  const [ count, setCount ] = useState( 0 )
+
 
   return (
-    <h1>Hello</h1>
+    <div className="App">
 
-  )
+      <Navigation />
+      <div>
+        home
+      </div>
+    </div>
+
+  );
 }
 
-export default App
+export default App; 
